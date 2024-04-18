d35c7208628cc5742cc018ebc259d72deeeb5955
sawsa <asaw1@sheffield.ac.uk>
2024-04-18 19:40:56 +0100

    implemented polynomial regression that can be run from main - not sure if it works correctly yet but it does run

--- Main.py
+++ Main.py
@@ -3,47 +3,16 @@ from matplotlib import pyplot as plt
 from src.fitness_calculator import BeamngFitnessCalc
 from src.test_case import TestCase
 #from src import Database
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-import operator
+from src.surrogate_model import polynomial_regression
 
 if __name__ == '__main__':
-    # Generate data
-    np.random.seed(0)
-    x = 2 - 3 * np.random.normal(0, 1, 20)
-    y = x - 2 * (x ** 2) + 0.5 * (x ** 3) + np.random.normal(-3, 3, 20)
-    x = x[:, np.newaxis]
-    y = y[:, np.newaxis]
-
-    # Polynomial features
-    degree = 6
-    poly_features = PolynomialFeatures(degree=degree)
-    x_poly = poly_features.fit_transform(x)
-
-    # Regression model
-    model = LinearRegression()
-    model.fit(x_poly, y)
-    y_poly_pred = model.predict(x_poly)
-
-    # Evaluation
-    rmse = np.sqrt(mean_squared_error(y, y_poly_pred))
-    r2 = r2_score(y, y_poly_pred)
-    print(f"Root Mean Squared Error: {rmse}")
-    print(f"R^2 Score: {r2}")
-
-    # Plot
-    sort_axis = operator.itemgetter(0)
-    sorted_zip = sorted(zip(x, y_poly_pred), key=sort_axis)
-    x, y_poly_pred = zip(*sorted_zip)
-
-    plt.scatter(x, y, color='red')
-    plt.plot(x, y_poly_pred, color='blue')
-    plt.title('Polynomial Regression')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+    testcase1 = TestCase([[10, 10], [50, 50], [60, 60]], [0.7], [], [])
+    testcase2 = TestCase([[10, 10], [50, 50], [5, 60]], [0.3], [], [])
+    testcase3 = TestCase([[10, 10], [50, 50], [5, 150]], [0.5], [], [])
+    test_cases = [testcase1,testcase2,testcase3]
+    surrogate = polynomial_regression()
+    surrogate.train(test_cases)
+    surrogate.test()
 
     #fit = BeamngFitnessCalc("Beamng")
     #testcase1 = TestCase([[10, 10], [50, 50], [60, 60]], [], [], [])
--- src/surrogate_model.py
+++ src/surrogate_model.py
@@ -1,28 +1,55 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-import operator
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
 
 class AbstractSurrogateModel(ABC):
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
     @abstractmethod
-    def train(self, x, y):
+    def train(self, test_cases):
         pass
 
     @abstractmethod
-    def predict(self, x):
+    def predict(self, test_cases):
         pass
 
 class polynomial_regression(AbstractSurrogateModel):
 
-    def train(self, x, y):
+    def __init__(self, degree=2):
+        self.degree = degree
+        self.model = LinearRegression()
+        self.X_test = []
+        self.y_test = []
 
-    def predict(self, x):
+    def reformat_represent(self, testcase):
+        representation = testcase.get_representation()
+        arr = np.array(representation)
+        flat_arr = arr.reshape(-1)
+        return flat_arr
 
+    def train(self, test_cases):
+        x = []
+        y = []
+        for test_case in test_cases:
+            sample = self.reformat_represent(test_case)
+            x.append(sample)
+            fitness_score_sim = test_case.get_fitness_score_sim()[0]
+            y.append(fitness_score_sim)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+        self.X_test = X_test
+        self.y_test = y_test
+        self.model = make_pipeline(PolynomialFeatures(self.degree), LinearRegression())
+        self.model.fit(X_train, y_train)
+
+    def test(self):
+        predict = self.model.predict(self.X_test)
+        mse = mean_squared_error(self.y_test, predict)
+        print(f'Mean Squared Error: {mse}')
+    def predict(self, testcase):
+        prediction = self.model.predict([testcase])
+        return prediction[0]
